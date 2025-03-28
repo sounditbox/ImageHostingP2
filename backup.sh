2475bcb8e2292718465e8c5b89ebd11bd283f809
Ruslan <sounditbox@gmail.com>
2025-03-28 20:40:53 +0400

    Refactored backup script

--- backup.sh
+++ backup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-BACKUP_DIR=backups
+BACKUP_DIR=${BACKUP_DIR:='./backups'}
+PG_USER=${PG_USER:='postgres'}
+DB_NAME=${DB_NAME:='images_db'}
 NOW=$(date '+%Y-%m-%d %H%M%S')
 FILE_NAME=backup_$NOW.sql
 
 mkdir -p "$BACKUP_DIR"
 
 echo "Creating backup..."
-docker exec -t db pg_dump -U postgres images_db > "$BACKUP_DIR/$FILE_NAME" && echo "Backup $FILE_NAME created" || echo "Backup $FILE_NAME failed"
+docker exec -t db pg_dump -U  "$PG_USER" $DB_NAME > "$BACKUP_DIR/$FILE_NAME" && echo "Backup $FILE_NAME created" || echo "Backup $FILE_NAME failed"
